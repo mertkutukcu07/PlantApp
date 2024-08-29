62b5516ba1de59ea6e98304acadccba57517e768
Mert Kütükçü <mertkutukcu07@icloud.com>
2024-08-29 13:39:33 +0300

    fix: delete isonboardingcompleted selector

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -5,8 +5,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { RouteNames } from './RouteNames';
 import { OnboardingStack, TabStack } from './stacks';
 import { SplashScreen } from '~/screens';
-import { useSelector } from 'react-redux';
-import { RootState } from '~/redux/store';
 
 export type AppNavigatorParamList = {
   [RouteNames.SPLASH]: undefined;
@@ -17,7 +15,6 @@ export type AppNavigatorParamList = {
 const Stack = createStackNavigator<AppNavigatorParamList>();
 
 export default function AppStack() {
-  const { isOnboardingCompleted } = useSelector((state: RootState) => state.onboarding);
   return (
     <Stack.Navigator
       screenOptions={{
@@ -26,11 +23,8 @@ export default function AppStack() {
       }}
       initialRouteName={RouteNames.SPLASH}>
       <Stack.Screen name={RouteNames.SPLASH} component={SplashScreen} />
-      {isOnboardingCompleted ? (
-        <Stack.Screen name={RouteNames.TABSTACK} component={TabStack} />
-      ) : (
-        <Stack.Screen name={RouteNames.ONBOARDINGSTACK} component={OnboardingStack} />
-      )}
+      <Stack.Screen name={RouteNames.ONBOARDINGSTACK} component={OnboardingStack} />
+      <Stack.Screen name={RouteNames.TABSTACK} component={TabStack} />
     </Stack.Navigator>
   );
 }
